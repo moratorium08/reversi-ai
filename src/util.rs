0a238d8b4fe96baa432c7ef887fd6ba34dbfaaec
moratorium08 <moratorium08@gmail.com>
2018-08-04 14:14:25 +0900

    Fix leading zeros

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,5 @@
 // bit utils
+#[inline(always)]
 pub fn clz(x: u64) -> u8 {
-    let mut mask = 1u64 << 63;
-    let mut cnt = 0u8;
-    while mask > 0 {
-        if mask & x != 0 {
-            return cnt;
-        }
-        cnt += 1;
-        mask >>= 1;
-    }
-    63
+    x.leading_zeros() as u8
 }
