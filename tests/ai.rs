90d964acb4ccdc82eaaf3ceee4e3296328bc0d69
moratorium08 <moratorium08@gmail.com>
2018-08-06 00:33:02 +0900

    oops

--- tests/ai.rs
+++ tests/ai.rs
@@ -9,7 +9,7 @@ use client::player::winnable_solver::{MatchResult, winnable};
 fn test_winnable() {
     let h = board::Hash::from_values(0x0000044c5e3e0300, 0xfffcf8b0a0c0c0e7);
     let board = board::Board::from_hash(h);
-    match winnable(board, color::Color::black()) {
+    match winnable(board, color::Color::black(), false) {
         MatchResult::Win(_) => (),
         _ => panic!("Failed: winnable")
     };
